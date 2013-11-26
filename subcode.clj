ec94888d40aa7c3ab3bc5c29dfd272c1fd88704e
rizaudo <hosoyakei@gmail.com>
2013-11-26 19:35:58 +0900

    fix NOTIFY function

--- subcode.clj
+++ subcode.clj
@@ -8,9 +8,14 @@
 (defn NOTIFY [sender event & ref]
   ;;後で改行コード入れること
   (str
-   "NOTIFY SSTP/1.0"
-   "Sender: " sender
-   "Event: " event
-   (doseq [[i x] (indexd ref)]
-     (str "Reference" i ":" x))
-       ))
+   "NOTIFY SSTP/1.0\r\n"
+   "Sender: " sender "\r\n"
+   "Event: " event "\r\n"
+   (cond
+    (nil? (seq ref)) ""
+    (< 7 (count ref)) (throw (llegalArgumentException. "wrong number of arguments"))
+    :default
+    (doseq [[i x] (indexd ref)]
+      (str "Reference" i ":" x "\r\n")))))
+
+;;; (.write w (NOTIFY hogehoge ) ) (.flush w ) 
